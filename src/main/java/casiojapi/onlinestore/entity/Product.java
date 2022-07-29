84a8fd92a876d42ae0a2a0829197fd293786480a
casiojapi <maximosantoro@protonmail.com>
2022-07-29 20:39:56 -0300

    create product entity

---
+++ src/main/java/casiojapi/onlinestore/entity/Product.java
@@ -0,0 +1,54 @@
+package casiojapi.onlinestore.entity;
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Basic(optional = false)
+    private String name;
+
+    private Double price;
+
+    private String pictureUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
